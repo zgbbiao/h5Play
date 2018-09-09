ab1359579ae24c0f8fd7ee79a54a8cea03bf6df5
zgbbiao <1031516418@qq.com>
2018-09-09 18:10:16 +0800

    '加载资源2'

--- js/game.js
+++ js/game.js
@@ -35,35 +35,16 @@
     }
 
     function morioRenderObj (sc) {
-        var runFrames = new Frames('m_run', document.querySelector('#mr'))
-        var jumpFrames = new Frames('m_jump', document.querySelector('#mr'))
-        var crouchFrames = new Frames('m_crouch', document.querySelector('#mr'))
-		// 跑步的
-		var arr = [1,2,3]
-        // arr.forEach(function (item, i) {
-        //     runFrames.add(32 * i, 0, 32, 32)
-        // })
-
-		// 添加跳跃
-        // jumpFrames.add(0, 0, 32, 32)
-        // jumpFrames.add(32*6, 0, 32, 32)
-
-		// 添加蹲下帧
-        // crouchFrames.add(0, 0, 32, 32)
-        // crouchFrames.add(32*14, 0, 32, 32)
-
 		// 创建动画集合
         var anims = ResManager.getAnimationsByName("sprite","Mario");
-        // anims.add('run', runFrames, true)
-        // anims.add('jump', jumpFrames)
-        // anims.add('crouch', crouchFrames)
 		for (var i = 0; i< 10; i ++) {
-            // var mr = sc.createRObj(Mario.ClassName, ['mr'])
-            // mr.moveTo(i * (mr.owner.w / 10), MathUtil.randInt(0, 30))
-			// mr.dy = MathUtil.randInt(1,3);
-            // mr.setAnimSpeed(2 * Math.random())
-			// mr.w = mr.h = 64
-			// mr.setAnims(anims, 'run')
+            var mr = sc.createRObj(Mario.ClassName, ['mr'])
+            mr.moveTo(i * (mr.owner.w / 10), MathUtil.randInt(0, 30))
+			mr.dy = MathUtil.randInt(1,3);
+            mr.setAnimSpeed(2 * Math.random())
+			mr.w = mr.h = 64
+			// 执行相关配置的动画
+			mr.setAnims(anims, 'run')
 		}
 	}
     // 初始化游戏
