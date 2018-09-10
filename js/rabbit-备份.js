58250bdc70bc3750f2f1b8b2b9a603d741bbfb24
zgbbiao <1031516418@qq.com>
2018-09-10 13:37:04 +0800

    实现兔子移动和平均分配物品--正在修改碰撞检测

--- js/rabbit-备份.js
+++ js/rabbit-备份.js
@@ -5,8 +5,56 @@
         //重新update
         update: function () {
             this._super()
+            var so = this
             if (!StickGame.selObj) {
-                StickGame.selObj = this
+                StickGame.selObj = so
+            }
+            var sc = StickGame.sceneManager.getScene("main");
+            //检测是否和其他精灵相交
+            var cobj = sc.rObjs;
+            var o = null,
+                io = null;
+            var x1 = so.x-so.w*0.5,
+                y1 = so.y-so.h*0.5,
+                x2 = so.x+so.w*0.5,
+                y2 = so.y+so.h*0.5,
+                x3,y3,x4,y4;
+            for(var i=0;i<cobj.length;i++)
+            {
+                o = cobj[i];
+                if(o.name!=so.name)
+                {
+                    x3 = o.x-o.w*0.5;
+                    y3 = o.y-o.h*0.5;
+                    x4 = o.x+o.w*0.5;
+                    y4 = o.y+o.h*0.5;
+                    if(MathUtil.isInRect(x1,y1,x2,y2,x3,y3,x4,y4))
+                    {
+                        io = o;
+                    }
+                }
+                if(o!=so&& so &&so.isCollide(o))
+                {
+                    console.log(sc.rObjs)
+                    // so.owner.removeRObj(o)
+                    console.log("发生碰撞")
+                    return false
+                }
+            }
+            if(io!=null)
+            {
+                // //获取相交区域
+                // var rc = MathUtil.getInRect(x1,y1,x2,y2,x3,y3,x4,y4);
+                // //绘制相交区域到另一个canvas中
+                // var ctx = dRect.getContext("2d");
+                // var sctx = sc.cvs[0];
+                // var w = rc[2]-rc[0],
+                //     h = rc[3]-rc[1];
+                // if(w>0&&h>0)
+                // {
+                //     ctx.clearRect(0,0,200,200);
+                //     ctx.drawImage(sctx,rc[0],rc[1],w,h,(200-4*w)*0.5,(200-4*h)*0.5,4*w,4*h);
+                // }
             }
         },
         //检测是否和其他精灵碰撞
